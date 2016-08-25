727d643901fb9143fea8daea6ccaf761405e32dd
KyoungRan <kyoungrankang@gmail.com>
2016-08-26 00:21:13 +0900

    change the S3(Home)

--- src/app/components/Home/SectionThree.js
+++ src/app/components/Home/SectionThree.js
@@ -13,10 +13,10 @@ class SectionThree extends Component {
                   <img id="section-three-person" src="../app/images/Home/person.png" />
                   <img src="../app/images/Home/XMLID_13_.png" />
                 </div>
-                <h1><strong>STEP </strong><img id="section-three-step3" src="../app/images/Home/1.png" /></h1>
+                <img id="section-three-step3" src="../app/images/Home/step1.png" />
               </div>
               <div className="col-md-7 section-three-step3">
-                <h1><img src="../app/images/Home/XMLID_271_.png" /><strong> Be Informed</strong></h1>
+                <img src="../app/images/Home/beinformed.png" />
               </div>
             </div>
             <div className="row">
@@ -25,10 +25,10 @@ class SectionThree extends Component {
                   <img id="section-three-person" src="../app/images/Home/person.png" />
                   <img src="../app/images/Home/XMLID_13_.png" />
                 </div>
-                <h1><strong>STEP </strong><img id="section-three-step3" src="../app/images/Home/2.png" /></h1>
+                <img id="section-three-step3" src="../app/images/Home/step2.png" />
               </div>
               <div className="col-md-7 section-three-step3">
-                <h1><img src="../app/images/Home/XMLID_4053_.png" /><strong> Make a Plan</strong></h1>
+                <img src="../app/images/Home/makeaplan.png" />
               </div>
             </div>
             <div className="row">
@@ -37,37 +37,29 @@ class SectionThree extends Component {
                   <img id="section-three-person" src="../app/images/Home/person.png" />
                   <img src="../app/images/Home/XMLID_13_.png" />
                 </div>
-                <h1><strong>STEP </strong><img id="section-three-step3" src="../app/images/Home/3.png" /></h1>
+                <img id="section-three-step3" src="../app/images/Home/step3.png" />
               </div>
               <div className="col-md-7 section-three-step3">
-                <h1><img src="../app/images/Home/XMLID_4041_.png" /><strong> Get a Kit</strong></h1>
+                <img src="../app/images/Home/getakit.png" />
               </div>
-            </div>
-            <div className="col-md-offset-3 col-md-2">
-              <div className="verticalLine"></div>
             </div>
             <div className="row">
-              <div className="col-md-offset-2 col-md-9 section-three-images">
-                <img src="../app/images/Home/svcguide.png" />
-                <img src="../app/images/Home/XMLID_225_.png" />
+              <div className="col-md-offset-3 col-md-2">
+                <img id="section-three-hooktext1" src="../app/images/Home/svcguide.png" />
+              </div>
+              <div className="col-md-2">
+                <img id="" src="../app/images/Home/hook.png" />
+              </div>
+              <div className="col-md-5">
+                <img id="section-three-hooktext2" src="../app/images/Home/XMLID_225_.png" />
+                <img id="section-three-hooktext3" src="../app/images/Home/disastersafe.png" />
               </div>
             </div>
-            <div className="col-md-offset-3 col-md-2">
-              <div className="verticalLine-middle-grey"></div>
-              <div className="verticalLine-small-black"></div>
-              <h2 id="section-three-h2"><strong>For</strong></h2>
-              <div className="verticalLine-small-black"></div>
-              <div className="verticalLine-middle-grey-last section-three-images">
-                <img src="../app/images/Home/svcguide.png" />
-              </div>
-            </div>
-
           </div>
         </section>
       </div>
     )
   }
 }
-{/*<h1><strong>STEP </strong><img src="../app/images/Home/2.png" /></h1>
-<img src="../app/images/Home/XMLID_13_.png" /><h1><strong>STEP </strong><img src="../app/images/Home/3.png" /></h1>*/}
+
 export default SectionThree;
